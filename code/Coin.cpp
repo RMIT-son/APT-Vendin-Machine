c4ca723b2596984e0b7f28d894a35667740dd297
Do Thuy Linh <s3927777@student.rmit.edu.au>
2024-05-12 23:00:35 +1000

    Update

--- code/Coin.cpp
+++ code/Coin.cpp
@@ -1,6 +1,6 @@
 #include "Coin.h"
  
- // implement functions for managing coins; this may depend on your design.
+ // implement functions for managing coins
  Coin::Coin(Denomination denom, unsigned count) {
      this->denom = denom;
      this->count = count;
