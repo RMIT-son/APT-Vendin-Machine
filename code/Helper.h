e3918caa83fed72aa86ac2a6dcb4244a2affa166
RMIT-son <119044913+RMIT-son@users.noreply.github.com>
2024-05-14 00:42:26 +1000

    Fixed Coins and added CoinManager and I/O tested and true

--- code/Helper.h
+++ code/Helper.h
@@ -13,7 +13,20 @@
 
 class Helper {
 public:
+    /**
+     * @brief Read a price from a string
+     * @details This function will read a price from a string.
+     * @param priceStr The string to read the price from.
+     * @return The price read from the string.
+     */
     static Price readPrice(const std::string& priceStr);
+
+    /**
+     * @brief Convert a price to a string
+     * @details This function will convert a price to a string.
+     * @param price The price to convert to a string.
+     * @return The string representation of the price.
+     */
     static std::string priceToString(const Price& price);
 };
 
